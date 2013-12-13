c666401a1322f39e5d9d48bb33e55c7932460c0f
edan@edan.org <edan>
2013-12-12 21:53:46 -0500

    adding minitest and tests fixing exceptions

--- lib/faraday/newscred.rb
+++ lib/faraday/newscred.rb
@@ -2,9 +2,9 @@ require 'faraday'
 module Faraday
 
   class Newscred < Faraday::Middleware
-    VALID_API_PARAMS  = [:access_key, :format, :pagesize, :offset, :fields]
-    VALID_FORMATS     = [:xml, :json, :rss]
-    MAX_PAGESIZE      = 999
+    VALID_API_PARAMS  = [:access_key, :format, :pagesize, :offset, :fields, :from_date, :to_date, :query].freeze
+    VALID_FORMATS     = [:xml, :json, :rss].freeze
+    MAX_PAGESIZE      = 999.freeze
 
     attr_reader :newscred_params
     
@@ -12,7 +12,7 @@ module Faraday
       super(app)
       assert_valid_options!(newscred_params)
 
-      raise ArgumentError, "access_key can't be nil" if newscred_params[:access_key].nil?
+      raise ArgumentError, 'access_key can not be nil, please pass in access_key in options hash' if newscred_params[:access_key].nil?
       @newscred_params = newscred_params
     end
 
@@ -26,7 +26,7 @@ module Faraday
     def assert_valid_options!(options)
       options.each_key do |key|
         unless VALID_API_PARAMS.include?(key)
-          raise ArgumentError.new("Unknown option: #{key}. Valid options are :#{VALID_OPTIONS.join(', ')}")
+          raise ArgumentError.new("Unknown api params: #{key}. Valid api params are: [#{VALID_API_PARAMS.join(', ')}]")
         end
       end
     end
---
+++ test/newscred_test.rb
@@ -0,0 +1,76 @@
+gem 'minitest' if defined? Bundler
+require 'minitest/autorun'
+
+require 'faraday/newscred'
+
+class FaradayNewscredTest < MiniTest::Unit::TestCase
+
+  def setup
+    @url = 'http://newscred.example.com/articles' 
+    @optional_api_params = {
+      :format    => :json, 
+      :pagesize  => 10, 
+      :offset    => 2, 
+      :fields    => "article.title", 
+      :from_date => "12-02-2013",
+      :to_date   => "12-03-2013",
+      :query     => "fun"
+    } 
+  end
+
+  def conn
+    Faraday::Connection.new('http://example.net/') do |builder|
+      yield builder
+      builder.adapter :test do |stub|
+        stub.get('/newscred_path') do |env|
+          [200, {}, env[:url].query]
+        end
+      end
+    end
+  end
+
+  def test_does_not_allow_invalid_api_params
+    no_access_key = lambda { 
+      conn do |b|
+        b.use Faraday::Newscred, {:access_key => "some_key", :invalid_key => "i am not valid"} 
+      end.get("/newscred_path") 
+    }
+    no_access_key.must_raise(ArgumentError)
+    error = no_access_key.call rescue $!
+    assert_equal 'Unknown api params: invalid_key. '\
+      'Valid api params are: '\
+      '[access_key, format, pagesize, offset, fields, from_date, to_date, query]', error.message
+  end
+
+  def test_raises_if_access_key_missing
+    no_access_key = lambda { conn { |b| b.use Faraday::Newscred }.get('/newscred_path')}
+    no_access_key.must_raise(ArgumentError)
+    error = no_access_key.call rescue $!
+    assert_equal 'access_key can not be nil, please pass in access_key in options hash', error.message
+  end
+
+  def test_adds_access_key_to_query_string
+    response = conn { |b|
+      b.use Faraday::Newscred, {:access_key => "some_key"}
+    }.get('/newscred_path')
+
+    assert_equal("access_key=some_key", response.body)
+  end
+
+  def test_adds_all_optional_newscred_api_params
+    response = conn { |b|
+      b.use Faraday::Newscred, {:access_key => "some_key"}.update(@optional_api_params)
+    }.get('/newscred_path')
+
+    assert_equal(
+      "access_key=some_key&"\
+      "format=json&"\
+      "pagesize=10&"\
+      "offset=2&"\
+      "fields=article.title&"\
+      "from_date=12-02-2013&"\
+      "to_date=12-03-2013&"\
+      "query=fun", response.body)
+  end
+
+end
